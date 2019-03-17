2a5769ddfb45d5020f519cfe4698c9b9ba5f3eeb
Rajkumar <rajkumaar2304@gmail.com>
2019-03-17 12:01:23 +0530

    Adds code for retrieving AAID
    Signed-off-by: Rajkumar <rajkumaar2304@gmail.com>

--- app/src/main/java/in/co/rajkumaar/amritarepo/helpers/Utils.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/helpers/Utils.java
@@ -25,9 +25,19 @@
 package in.co.rajkumaar.amritarepo.helpers;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.Looper;
+import android.os.Parcel;
+import android.os.RemoteException;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
@@ -38,13 +48,16 @@ import android.widget.Toast;
 import com.facebook.ads.AdSettings;
 import com.facebook.ads.AdSize;
 import com.facebook.ads.AdView;
+import com.facebook.ads.AudienceNetworkAds;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Objects;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import in.co.rajkumaar.amritarepo.R;
 
@@ -82,7 +95,8 @@ public class Utils {
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
     }
 
-    private static void showAd(Context context, LinearLayout linearLayout, AdSize adSize){
+    private static void showAd(final Context context, LinearLayout linearLayout, AdSize adSize){
+        AudienceNetworkAds.initialize(context);
         AdView adView = new AdView(context, context.getString(R.string.placement_id), adSize);
         linearLayout.addView(adView);
         adView.loadAd();
@@ -106,5 +120,112 @@ public class Utils {
         years.add((currentYear)  +"_"+String.valueOf(currentYear+1).substring(2,4));
         years.add((currentYear+1)+"_"+String.valueOf(currentYear+2).substring(2,4));
         return years;
+    }
+
+    public static void printAAID(final Context context){
+        new Thread(new Runnable() {
+            public void run() {
+                if(Looper.myLooper() == Looper.getMainLooper()) throw new IllegalStateException("Cannot be called from the main thread");
+
+                try { PackageManager pm = context.getPackageManager(); pm.getPackageInfo("com.android.vending", 0); }
+                catch (Exception e) {e.printStackTrace();}
+
+                AdvertisingConnection connection = new AdvertisingConnection();
+                Intent intent = new Intent("com.google.android.gms.ads.identifier.service.START");
+                intent.setPackage("com.google.android.gms");
+                if(context.bindService(intent, connection, Context.BIND_AUTO_CREATE)) {
+                    try {
+                        AdvertisingInterface adInterface = new AdvertisingInterface(connection.getBinder());
+                        AdInfo adInfo = new AdInfo(adInterface.getId(), adInterface.isLimitAdTrackingEnabled(true));
+                        Log.v("ADS-ID",adInfo.getId());
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    } finally {
+                        context.unbindService(connection);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public static final class AdInfo {
+        private final String advertisingId;
+        private final boolean limitAdTrackingEnabled;
+
+        AdInfo(String advertisingId, boolean limitAdTrackingEnabled) {
+            this.advertisingId = advertisingId;
+            this.limitAdTrackingEnabled = limitAdTrackingEnabled;
+        }
+
+        public String getId() {
+            return this.advertisingId;
+        }
+
+        public boolean isLimitAdTrackingEnabled() {
+            return this.limitAdTrackingEnabled;
+        }
+    }
+
+    private static final class AdvertisingConnection implements ServiceConnection {
+        boolean retrieved = false;
+        private final LinkedBlockingQueue<IBinder> queue = new LinkedBlockingQueue<IBinder>(1);
+
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            try { this.queue.put(service); }
+            catch (InterruptedException localInterruptedException){}
+        }
+
+        public void onServiceDisconnected(ComponentName name){}
+
+        public IBinder getBinder() throws InterruptedException {
+            if (this.retrieved) throw new IllegalStateException();
+            this.retrieved = true;
+            return (IBinder)this.queue.take();
+        }
+    }
+
+    private static final class AdvertisingInterface implements IInterface {
+        private IBinder binder;
+
+        public AdvertisingInterface(IBinder pBinder) {
+            binder = pBinder;
+        }
+
+        public IBinder asBinder() {
+            return binder;
+        }
+
+        public String getId() throws RemoteException {
+            Parcel data = Parcel.obtain();
+            Parcel reply = Parcel.obtain();
+            String id;
+            try {
+                data.writeInterfaceToken("com.google.android.gms.ads.identifier.internal.IAdvertisingIdService");
+                binder.transact(1, data, reply, 0);
+                reply.readException();
+                id = reply.readString();
+            } finally {
+                reply.recycle();
+                data.recycle();
+            }
+            return id;
+        }
+
+        public boolean isLimitAdTrackingEnabled(boolean paramBoolean) throws RemoteException {
+            Parcel data = Parcel.obtain();
+            Parcel reply = Parcel.obtain();
+            boolean limitAdTracking;
+            try {
+                data.writeInterfaceToken("com.google.android.gms.ads.identifier.internal.IAdvertisingIdService");
+                data.writeInt(paramBoolean ? 1 : 0);
+                binder.transact(2, data, reply, 0);
+                reply.readException();
+                limitAdTracking = 0 != reply.readInt();
+            } finally {
+                reply.recycle();
+                data.recycle();
+            }
+            return limitAdTracking;
+        }
     }
 }
