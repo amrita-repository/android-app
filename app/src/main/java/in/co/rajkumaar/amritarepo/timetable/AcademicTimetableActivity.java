7516dc48dfce5a2efe980b51a48d3ea9902ce747
Sanjai Lal T S <38038951+sanjailal@users.noreply.github.com>
2020-03-25 17:11:12 +0530

    Removed harcoded department and branch names :wink: (#19)
    Signed-off-by: Sanjai Lal <sanjailal4@gmail.com>
    
    Co-Authored-By: Rajkumar S <rajkumaar2304@icloud.com>

--- app/src/main/java/in/co/rajkumaar/amritarepo/timetable/AcademicTimetableActivity.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/timetable/AcademicTimetableActivity.java
@@ -25,6 +25,7 @@
 package in.co.rajkumaar.amritarepo.timetable;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -32,18 +33,32 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import cz.msebera.android.httpclient.Header;
 import in.co.rajkumaar.amritarepo.R;
 import in.co.rajkumaar.amritarepo.activities.WebViewActivity;
 import in.co.rajkumaar.amritarepo.helpers.DownloadTask;
@@ -58,10 +73,13 @@ public class AcademicTimetableActivity extends AppCompatActivity {
     public Spinner branch;
     public Spinner sem;
     public Spinner batch;
+    private List<String> courses = new ArrayList<>();
+    private List<String> branches = new ArrayList<>();
     private List<String> batches = new ArrayList<>();
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
 
+    @SuppressLint("CommitPrefEdits")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,19 +97,23 @@ public class AcademicTimetableActivity extends AppCompatActivity {
         pref = getSharedPreferences("student_timetable", MODE_PRIVATE);
         editor = pref.edit();
         new clearCache().clear(this);
-
-
         year = findViewById(R.id.acad_year);
         course = findViewById(R.id.acad_course);
         branch = findViewById(R.id.acad_branch);
         sem = findViewById(R.id.acad_sem);
         batch = findViewById(R.id.acad_batch);
 
+
         loadLists();
         loadFromPref();
         course.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Gson gson = new Gson();
+                List<String> dummyDisplay = new ArrayList<>();
+                dummyDisplay.add("[Choose branch]");
+                String dummyDisplayJson = gson.toJson(dummyDisplay);
+                pref.edit().putString("[Choose course]", dummyDisplayJson).apply();
                 buildBranchesSpinner(position);
             }
 
@@ -181,97 +203,39 @@ public class AcademicTimetableActivity extends AppCompatActivity {
     }
 
     private void buildBranchesSpinner(int courseID) {
-        List<String> branches = new ArrayList<>();
-        branches.add("[Choose Branch]");
-        switch (courseID) {
-            case 1:
-                branches.add("AEE");
-                branches.add("AIE");
-                branches.add("CHE");
-                branches.add("CIE");
-                branches.add("CVI");
-                branches.add("CSE");
-                branches.add("ECE");
-                branches.add("EEE");
-                branches.add("ELC");
-                branches.add("EIE");
-                branches.add("EAC");
-                branches.add("CCE");
-                branches.add("MEE");
-                break;
-            case 2:
-                branches.add("MAC");
-                branches.add("ENG");
-                break;
-            case 3:
-                branches.add("CHE");
-                branches.add("MAT");
-                branches.add("PHY");
-                branches.add("DAS");
-                break;
-            case 4:
-                branches.add("ATE");
-                branches.add("ATL");
-                branches.add("BME");
-                branches.add("CEN");
-                branches.add("CHE");
-                branches.add("CIE");
-                branches.add("CSE");
-                branches.add("CSP");
-                branches.add("CVI");
-                branches.add("CYS");
-                branches.add("EBS");
-                branches.add("EDN");
-                branches.add("MFG");
-                branches.add("MSE");
-                branches.add("PWE");
-                branches.add("RET");
-                branches.add("RSW");
-                branches.add("SCE");
-                branches.add("VLD");
-                break;
-            case 5:
-                branches.add("CMN");
-                branches.add("MAC");
-                branches.add("ENG");
-                break;
-            case 6:
-                branches.add("MBA");
-                break;
-            case 7:
-                branches.add("MCA");
-                break;
-            case 8:
-                branches.add("MSW");
-                break;
-            case 9:
-                branches.add("JLM");
-                break;
-        }
-        ArrayAdapter<String> courseAdapter = new ArrayAdapter<>(this, R.layout.spinner_item1, branches);
-        courseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        branch.setAdapter(courseAdapter);
-        if (course.getSelectedItemPosition() == pref.getInt("course", 0)) {
-            branch.setSelection(pref.getInt("branch", 0));
+        List<String> branchesTemp = new ArrayList<>();
+        branchesTemp.add("[Choose branch]");
+        if (!pref.contains(courses.get(courseID))) {
+            getBranches(courseID, branchesTemp, true);
+        } else {
+            branches = new ArrayList<>();
+            Gson gson = new Gson();
+            String json = pref.getString(courses.get(courseID), null);
+            Type listType = new TypeToken<ArrayList<String>>() {
+            }.getType();
+            branches.addAll((Collection<? extends String>) gson.fromJson(json, listType));
+            setBranchSpinner();
+            getBranches(courseID, branchesTemp, false);
         }
     }
 
-    private void loadLists() {
 
-        List<String> courses = new ArrayList<>();
-        courses.add("[Choose course]");
-        courses.add("BTech");
-        courses.add("BA");
-        courses.add("IMSc");
-        courses.add("MTech");
-        courses.add("MA");
-        courses.add("MBA");
-        courses.add("MCA");
-        courses.add("MSW");
-        courses.add("PGD");
-        ArrayAdapter<String> courseAdapter = new ArrayAdapter<String>(AcademicTimetableActivity.this, R.layout.spinner_item1, courses);
-        courseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        course.setAdapter(courseAdapter);
+    private void loadLists() {
+        List<String> coursesTemp = new ArrayList<>();
+        coursesTemp.add("[Choose course]");
+        if (!pref.contains("courses")) {
+            getCourse(coursesTemp, true);
+        } else {
+            courses = new ArrayList<>();
+            Gson gson = new Gson();
+            String json = pref.getString("courses", null);
+            Type listType = new TypeToken<ArrayList<String>>() {
+            }.getType();
+            courses.addAll((Collection<? extends String>) gson.fromJson(json, listType));
+            setCourseSpinner();
+            getCourse(coursesTemp, false);
+        }
+
 
         List<String> sems = new ArrayList<>();
         sems.add("[Choose semester]");
@@ -279,7 +243,7 @@ public class AcademicTimetableActivity extends AppCompatActivity {
         while (i <= 10) {
             sems.add(String.valueOf(i++));
         }
-        ArrayAdapter<String> semAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item1, sems);
+        ArrayAdapter<String> semAdapter = new ArrayAdapter<>(this, R.layout.spinner_item1, sems);
         semAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sem.setAdapter(semAdapter);
 
@@ -290,14 +254,95 @@ public class AcademicTimetableActivity extends AppCompatActivity {
         batches.add("D");
         batches.add("E");
         batches.add("F");
-        ArrayAdapter<String> batchAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item1, batches);
+        ArrayAdapter<String> batchAdapter = new ArrayAdapter<>(this, R.layout.spinner_item1, batches);
         batchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         batch.setAdapter(batchAdapter);
 
-        ArrayAdapter<String> yearAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item1, Utils.getAcademicYears());
+
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<>(this, R.layout.spinner_item1, Utils.getAcademicYears());
         yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         year.setAdapter(yearAdapter);
 
     }
 
+    private void setCourseSpinner() {
+        ArrayAdapter<String> courseAdapter = new ArrayAdapter<>(AcademicTimetableActivity.this, R.layout.spinner_item1, courses);
+        courseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        course.setAdapter(courseAdapter);
+    }
+
+    private void setBranchSpinner() {
+        ArrayAdapter<String> branchAdapter = new ArrayAdapter<>(this, R.layout.spinner_item1, branches);
+        branchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        branch.setAdapter(branchAdapter);
+        if (course.getSelectedItemPosition() == pref.getInt("course", 0)) {
+            branch.setSelection(pref.getInt("branch", 0));
+        }
+    }
+
+    private void getCourse(final List<String> coursesTemp, final boolean needUpdateCourse) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(getString(R.string.course_name_url), new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                Document html = Jsoup.parse(new String(bytes));
+                Element row = html.getElementById("drop_1");
+                Elements columns = row.getElementsByTag("option");
+                if (columns.get(0).text().equals("Course")) {
+                    for (int j = 1; j < columns.size(); j++) {
+                        String parsedCourse = columns.get(j).attr("value");
+                        coursesTemp.add(parsedCourse);
+                    }
+                }
+                Gson gson = new Gson();
+                String coursesJson = gson.toJson(coursesTemp);
+                pref.edit().putString("courses", coursesJson).apply();
+                if (needUpdateCourse) {
+                    courses = new ArrayList<>();
+                    courses.addAll(coursesTemp);
+                    setCourseSpinner();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Utils.showToast(AcademicTimetableActivity.this, "An unexpected error occurred. Please try again later");
+                finish();
+            }
+        });
+    }
+
+    private void getBranches(final int position, final List<String> branchesTemp, final boolean needUpdateBranch) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String html = getString(R.string.branch_name_url) + courses.get(position);
+        client.get(html, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                Document html = Jsoup.parse(new String(bytes));
+                Element row = html.getElementById("drop_2");
+                Elements columns = row.getElementsByTag("option");
+                for (int j = 1; j < columns.size(); j++) {
+                    String parsedCourse = columns.get(j).attr("value");
+                    branchesTemp.add(parsedCourse);
+                }
+                Gson gson = new Gson();
+                String branchJson = gson.toJson(branchesTemp);
+                pref.edit().putString(courses.get(position), branchJson).apply();
+                if (needUpdateBranch) {
+                    branches = new ArrayList<>();
+                    branches.addAll(branchesTemp);
+                    setBranchSpinner();
+                }
+            }
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Utils.showToast(AcademicTimetableActivity.this, "An unexpected error occurred. Please try again later");
+                finish();
+            }
+        });
+    }
+
 }
+
