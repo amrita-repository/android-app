ba7cc017581a8dcdf81ed767fb75e3f2f3b35c9b
Rajkumar S <rajkumaar2304@icloud.com>
2020-03-28 18:33:49 +0530

    Initiates the migration towards Volley
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>

--- app/src/main/java/in/co/rajkumaar/amritarepo/news/NewsClient.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/news/NewsClient.java
@@ -1,30 +1,17 @@
 /*
- * MIT License
- *
- * Copyright (c) 2019 RAJKUMAR S
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2020 RAJKUMAR S
  */
 
 package in.co.rajkumaar.amritarepo.news;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -33,44 +20,44 @@ import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 
-import cz.msebera.android.httpclient.Header;
-
 class NewsClient {
 
     private static String AMRITA_URL = "https://www.amrita.edu";
-    private final AsyncHttpClient client;
     private ArrayList<NewsItem> newsArticles;
     private String newsURL;
+    private RequestQueue requestQueue;
 
-    NewsClient() {
-        this.client = new AsyncHttpClient();
+    NewsClient(Context context) {
+        requestQueue = Volley.newRequestQueue(context);
         this.newsArticles = new ArrayList<>();
         this.newsURL = "https://www.amrita.edu/campus/Coimbatore/news";
     }
 
     void getArticles(final NewsResponse newsResponse) {
-        client.get(this.newsURL, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int i, Header[] headers, byte[] bytes) {
-                Document doc = Jsoup.parse(new String(bytes));
-                Elements articles = doc.select("article");
-                for (Element article : articles) {
-                    try {
-                        String imageUrl = article.select("img.img-responsive").first().attr("src");
-                        String title = article.select(".field-name-title").first().text();
-                        String url = AMRITA_URL + article.select(".field-name-node-link > div > div > a").first().attr("href");
-                        newsArticles.add(new NewsItem(imageUrl, title, url));
-                    } catch (Exception e) {
-                        newsResponse.onFailure(e);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, newsURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Document doc = Jsoup.parse(response);
+                        Elements articles = doc.select("article");
+                        for (Element article : articles) {
+                            try {
+                                String imageUrl = article.select("img.img-responsive").first().attr("src");
+                                String title = article.select(".field-name-title").first().text();
+                                String url = AMRITA_URL + article.select(".field-name-node-link > div > div > a").first().attr("href");
+                                newsArticles.add(new NewsItem(imageUrl, title, url));
+                            } catch (Exception e) {
+                                newsResponse.onFailure(e);
+                            }
+                        }
+                        newsResponse.onSuccess(newsArticles);
                     }
-                }
-                newsResponse.onSuccess(newsArticles);
-            }
-
+                }, new Response.ErrorListener() {
             @Override
-            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
-                newsResponse.onFailure(new Exception(throwable));
+            public void onErrorResponse(VolleyError error) {
+                newsResponse.onFailure(error);
             }
         });
+        requestQueue.add(stringRequest);
     }
 }
--- app/src/main/java/in/co/rajkumaar/amritarepo/opac/EResourcesActivity.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/opac/EResourcesActivity.java
@@ -1,10 +1,8 @@
 /*
- * Copyright (c) 2019 RAJKUMAR S
+ * Copyright (c) 2020 RAJKUMAR S
  */
 
 package in.co.rajkumaar.amritarepo.opac;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -14,8 +12,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -26,60 +30,61 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import cz.msebera.android.httpclient.Header;
 import in.co.rajkumaar.amritarepo.R;
 import in.co.rajkumaar.amritarepo.helpers.Utils;
 
 public class EResourcesActivity extends AppCompatActivity {
 
-    Map<String,String> links = new HashMap<>();
-    ArrayList<String> items = new ArrayList<>();
-    ListView listView;
+    private Map<String, String> links = new HashMap<>();
+    private ArrayList<String> items = new ArrayList<>();
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eresources);
         listView = findViewById(R.id.list);
-        if(getIntent().hasExtra("useful-sites")){
+        if (getIntent().hasExtra("useful-sites")) {
             getSupportActionBar().setTitle("Useful Websites");
         }
         fetchResources((getIntent().hasExtra("useful-sites")));
     }
 
-    void fetchResources(final boolean b){
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(getString(R.string.central_library), new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                try {
-                    Document doc = Jsoup.parse(new String(responseBody));
-                    Element table = doc.select("table").get(b ? 1 : 0);
-                    Elements anchors = table.select("a");
-                    for (Element a : anchors) {
-                        links.put(a.text(), a.attr("href"));
-                        items.add(a.text());
-                    }
-                    System.out.println(links.toString());
-                    ArrayAdapter arrayAdapter = new ArrayAdapter<>(EResourcesActivity.this, R.layout.eresources_item, R.id.text1, items);
-                    listView.setAdapter(arrayAdapter);
-                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                            Intent intent = new Intent(Intent.ACTION_VIEW);
-                            intent.setData(Uri.parse(links.get(items.get(position))));
-                            startActivity(intent);
+    void fetchResources(final boolean b) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, getString(R.string.central_library),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Document doc = Jsoup.parse(response);
+                            Element table = doc.select("table").get(b ? 1 : 0);
+                            Elements anchors = table.select("a");
+                            for (Element a : anchors) {
+                                links.put(a.text(), a.attr("href"));
+                                items.add(a.text());
+                            }
+                            ArrayAdapter arrayAdapter = new ArrayAdapter<>(EResourcesActivity.this, R.layout.eresources_item, R.id.text1, items);
+                            listView.setAdapter(arrayAdapter);
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                                    intent.setData(Uri.parse(links.get(items.get(position))));
+                                    startActivity(intent);
+                                }
+                            });
+                            findViewById(R.id.progressBar).setVisibility(View.GONE);
+                        } catch (Exception e) {
+                            Utils.showUnexpectedError(EResourcesActivity.this);
                         }
-                    });
-                    findViewById(R.id.progressBar).setVisibility(View.GONE);
-                }catch (Exception e){
-                    Utils.showUnexpectedError(EResourcesActivity.this);
-                }
-            }
-
+                    }
+                }, new Response.ErrorListener() {
             @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+            public void onErrorResponse(VolleyError error) {
                 Utils.showUnexpectedError(EResourcesActivity.this);
             }
         });
+        requestQueue.add(stringRequest);
     }
 }
