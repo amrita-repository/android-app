9b91ccabf97f79e30d572722aa21caf4fe38df26
Rajkumar S <rajkumaar2304@icloud.com>
2020-03-30 09:00:30 +0000

    Applies new logic in scraping exam schedules (#35)
    * Applies new logic in scraping exam schedules
    
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>
    
    * Codacy :(
    
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>
    
    * Bug fixes
    
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>

--- app/src/main/java/in/co/rajkumaar/amritarepo/examschedule/ExamCategoryActivity.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/examschedule/ExamCategoryActivity.java
@@ -4,22 +4,30 @@
 
 package in.co.rajkumaar.amritarepo.examschedule;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 import in.co.rajkumaar.amritarepo.R;
@@ -30,7 +38,6 @@ import in.co.rajkumaar.amritarepo.papers.PaperAdapter;
 public class ExamCategoryActivity extends BaseActivity {
 
     private String urlExams;
-    private ArrayList<String> headings;
     private ListView listView;
     private PaperAdapter scheduleBlockArrayAdapter;
 
@@ -40,80 +47,110 @@ public class ExamCategoryActivity extends BaseActivity {
         setContentView(R.layout.activity_exam_schedule);
 
         urlExams = getResources().getString(R.string.url_exams);
-        headings = new ArrayList<>();
-
         listView = findViewById(R.id.list);
         String quote = getResources().getStringArray(R.array.quotes)[new Random().nextInt(getResources().getStringArray(R.array.quotes).length)];
         ((TextView) findViewById(R.id.quote)).setText(quote);
-        new RetrieveSchedule().execute();
+
+        retrieveSchedules();
     }
 
-
-    @SuppressLint("StaticFieldLeak")
-    private class RetrieveSchedule extends AsyncTask<Void, Void, Void> {
-        private Document document = null;
-        private Elements titles;
-        private Elements ulLists;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                document = Jsoup.connect(urlExams).get();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-
-
-            try {
-                titles = document.select("div.field-items").select("p");
-                ulLists = document.select("div.field-items").select("ul");
-                Log.e("ELEMENTS ", String.valueOf(titles.size()));
-                for (int i = 0; i < titles.size(); ++i) {
-                    String spanstyle = titles.get(i).select("span[style]").attr("style");
-                    String head = titles.get(i).select("p").text().trim();
-                    if (head.length() > 0)
-                        headings.add(head);
-                } //Log.e("TEXT SIZE",String.valueOf(scheduleBlocks.get(1).getTexts().size()));
-
-
-                scheduleBlockArrayAdapter = new PaperAdapter(ExamCategoryActivity.this, headings, "examcategory");
-
-                scheduleBlockArrayAdapter.setCustomListener(new PaperAdapter.customListener() {
+    private void retrieveSchedules() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        final Type listOfExamItemType = new TypeToken<ArrayList<ExamItem>>() {
+        }.getType();
+        final Gson gson = new Gson();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlExams,
+                new Response.Listener<String>() {
                     @Override
-                    public void onItemClickListener(int i) {
-                        if (Utils.isConnected(ExamCategoryActivity.this))
-                            startActivity(new Intent(ExamCategoryActivity.this, ExamsListActivity.class).putExtra("block", i));
-                        else
-                            Utils.showInternetError(ExamCategoryActivity.this);
+                    public void onResponse(String response) {
+                        try {
+                            Document document = Jsoup.parse(response);
+                            Element firstTitle = document.select("div.field-items").select("p").first();
+                            Elements siblings = firstTitle.siblingElements();
+                            final HashMap<Integer, ArrayList<ExamItem>> examItems = new HashMap<>();
+                            ArrayList<ExamItem> examItemArrayList = new ArrayList<>();
+                            ArrayList<String> categories = new ArrayList<>();
+                            categories.add(firstTitle.text());
+                            int countIndex = 0;
+                            for (int i = 0; i < siblings.size(); i++) {
+                                Element sibling = siblings.get(i);
+                                if (!"p".equals(sibling.tagName())) {
+                                    if ("ul".equals(sibling.tagName())) {
+                                        Elements listItems = sibling.select("li");
+                                        for (Element item : listItems) {
+                                            examItemArrayList.add(
+                                                    new ExamItem(item.text().trim(), item.select("a[href]").attr("href"))
+                                            );
+                                        }
+                                    }
+                                } else {
+                                    examItems.put(countIndex, examItemArrayList);
+                                    examItemArrayList = new ArrayList<>();
+                                    if (sibling.select("a>span").attr("style").equals("color:#D3D3D3")) {
+                                        break;
+                                    }
+                                    categories.add(sibling.select("a>strong").text());
+                                    countIndex++;
+                                }
+                            }
+                            if (!examItemArrayList.isEmpty()) {
+                                examItems.put(countIndex, examItemArrayList);
+                            }
+
+                            scheduleBlockArrayAdapter = new PaperAdapter(ExamCategoryActivity.this, categories, "examcategory");
+                            scheduleBlockArrayAdapter.setCustomListener(new PaperAdapter.customListener() {
+                                @Override
+                                public void onItemClickListener(int i) {
+                                    if (Utils.isConnected(ExamCategoryActivity.this)) {
+                                        startActivity(
+                                                new Intent(ExamCategoryActivity.this, ExamsListActivity.class).putExtra("exams", gson.toJson(examItems.get(i), listOfExamItemType)));
+                                    } else {
+                                        Utils.showInternetError(ExamCategoryActivity.this);
+                                    }
+                                }
+                            });
+                            listView.setAdapter(scheduleBlockArrayAdapter);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Toast.makeText(ExamCategoryActivity.this, "Unexpected error. Please try again later", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                        findViewById(R.id.loading_indicator).setVisibility(View.GONE);
                     }
-                });
-                listView.setAdapter(scheduleBlockArrayAdapter);
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    }
-                });
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
                 Toast.makeText(ExamCategoryActivity.this, "Unexpected error. Please try again later", Toast.LENGTH_SHORT).show();
                 finish();
             }
-            findViewById(R.id.loading_indicator).setVisibility(View.GONE);
-            super.onPostExecute(aVoid);
-        }
+        });
+        requestQueue.add(stringRequest);
     }
 
+    public static class ExamItem {
+        private String title;
+        private String link;
+
+        private ExamItem(String title, String link) {
+            this.title = title;
+            this.link = link;
+        }
+
+        public String getLink() {
+            return link;
+        }
+
+        public void setLink(String link) {
+            this.link = link;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+    }
 }
--- app/src/main/java/in/co/rajkumaar/amritarepo/examschedule/ExamsListActivity.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/examschedule/ExamsListActivity.java
@@ -5,27 +5,20 @@
 package in.co.rajkumaar.amritarepo.examschedule;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 import in.co.rajkumaar.amritarepo.R;
 import in.co.rajkumaar.amritarepo.activities.BaseActivity;
@@ -36,130 +29,83 @@ import in.co.rajkumaar.amritarepo.papers.PaperAdapter;
 
 public class ExamsListActivity extends BaseActivity {
 
-    private String url_exams;
-
     private ArrayList<String> texts;
     private ArrayList<String> links;
-    private int block;
-    private PaperAdapter adapter;
     private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exams_under_each_dept);
-
-        url_exams = getResources().getString(R.string.url_exams);
         listView = findViewById(R.id.list);
-        block = getIntent().getExtras().getInt("block");
-        String quote = getResources().getStringArray(R.array.quotes)[new Random().nextInt(getResources().getStringArray(R.array.quotes).length)];
-        ((TextView) findViewById(R.id.quote)).setText(quote);
-
         texts = new ArrayList<>();
         links = new ArrayList<>();
-
-        new GetExams().execute();
+        ArrayList<ExamCategoryActivity.ExamItem> exams = new Gson().fromJson(getIntent().getStringExtra("exams")
+                , new TypeToken<ArrayList<ExamCategoryActivity.ExamItem>>() {
+                }.getType());
+        for (ExamCategoryActivity.ExamItem item : exams) {
+            texts.add(item.getTitle().replace("Download »", ""));
+            links.add(item.getLink());
+        }
+        populateExams();
     }
 
+    private void populateExams() {
 
-    @SuppressLint("StaticFieldLeak")
-    class GetExams extends AsyncTask<Void, Void, Void> {
-        private Document document = null;
-        private Elements ul_lists;
-
-        @Override
-        protected void onPreExecute() {
-            texts.clear();
-            links.clear();
-
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                document = Jsoup.connect(url_exams).get();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-
-            try {
-                ul_lists = document.select("div.field-items").select("ul");
-                for (int j = 0; j < ul_lists.get(block).select("li").size(); ++j) {
-
-                    String text = ul_lists.get(block).select("li").get(j).select("li").text();
-                    texts.add(text.trim());
-                    links.add(ul_lists.get(block).select("li").get(j).select("a[href]").attr("href"));
-                }
-                adapter = new PaperAdapter(ExamsListActivity.this, texts, "examlist");
-
-                adapter.setCustomListener(new PaperAdapter.customListener() {
+        PaperAdapter adapter = new PaperAdapter(ExamsListActivity.this, texts, "examlist");
+        adapter.setCustomListener(new PaperAdapter.customListener() {
+            @Override
+            public void onItemClickListener(final int i) {
+                final ArrayList<String> options = new ArrayList<>();
+                options.add("Open");
+                options.add("Download");
+                AlertDialog.Builder builder = new AlertDialog.Builder(ExamsListActivity.this);
+                ArrayAdapter<String> optionsAdapter = new ArrayAdapter<>(ExamsListActivity.this, android.R.layout.simple_list_item_1, options);
+                builder.setAdapter(optionsAdapter, new DialogInterface.OnClickListener() {
                     @Override
-                    public void onItemClickListener(final int i) {
-                        final ArrayList<String> qPaperOptions = new ArrayList<>();
-                        qPaperOptions.add("Open");
-                        qPaperOptions.add("Download");
-                        AlertDialog.Builder qPaperBuilder = new AlertDialog.Builder(ExamsListActivity.this);
-                        ArrayAdapter<String> optionsAdapter = new ArrayAdapter<String>(ExamsListActivity.this, android.R.layout.simple_list_item_1, qPaperOptions);
-                        qPaperBuilder.setAdapter(optionsAdapter, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int pos) {
-                                if (pos == 0) {
-                                    if (ContextCompat.checkSelfPermission(ExamsListActivity.this,
-                                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                            != PackageManager.PERMISSION_GRANTED) {
+                    public void onClick(DialogInterface dialogInterface, int pos) {
+                        if (pos == 0) {
+                            if (ContextCompat.checkSelfPermission(ExamsListActivity.this,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                    != PackageManager.PERMISSION_GRANTED) {
 
-                                        ActivityCompat.requestPermissions(ExamsListActivity.this,
-                                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                1);
-                                    } else {
-                                        if (Utils.isConnected(ExamsListActivity.this)) {
-                                            new OpenTask(ExamsListActivity.this, "https://intranet.cb.amrita.edu" + links.get(i), 2);
-                                        } else {
-                                            Utils.showInternetError(ExamsListActivity.this);
-                                        }
-
-
-                                    }
-                                } else if (pos == 1) {
-                                    if (ContextCompat.checkSelfPermission(ExamsListActivity.this,
-                                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                            != PackageManager.PERMISSION_GRANTED) {
-
-                                        ActivityCompat.requestPermissions(ExamsListActivity.this,
-                                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                1);
-                                    } else {
-                                        if (Utils.isConnected(ExamsListActivity.this)) {
-                                            new DownloadTask(ExamsListActivity.this, "https://intranet.cb.amrita.edu" + links.get(i), 2);
-                                        } else {
-                                            Utils.showInternetError(ExamsListActivity.this);
-                                        }
-
-
-                                    }
+                                ActivityCompat.requestPermissions(ExamsListActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        1);
+                            } else {
+                                if (Utils.isConnected(ExamsListActivity.this)) {
+                                    new OpenTask(ExamsListActivity.this, getString(R.string.intranet_url) + links.get(i), 2);
+                                } else {
+                                    Utils.showInternetError(ExamsListActivity.this);
                                 }
+
+
                             }
-                        });
-                        qPaperBuilder.show();
+                        } else if (pos == 1) {
+                            if (ContextCompat.checkSelfPermission(ExamsListActivity.this,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                    != PackageManager.PERMISSION_GRANTED) {
+
+                                ActivityCompat.requestPermissions(ExamsListActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        1);
+                            } else {
+                                if (Utils.isConnected(ExamsListActivity.this)) {
+                                    new DownloadTask(ExamsListActivity.this, getString(R.string.intranet_url) + links.get(i), 2);
+                                } else {
+                                    Utils.showInternetError(ExamsListActivity.this);
+                                }
 
 
+                            }
+                        }
                     }
                 });
-                listView.setAdapter(adapter);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(ExamsListActivity.this, "Unexpected error. Please try again later", Toast.LENGTH_SHORT).show();
-                finish();
-            }
+                builder.show();
 
-            findViewById(R.id.loading_indicator).setVisibility(View.GONE);
-            super.onPostExecute(aVoid);
-        }
+
+            }
+        });
+        listView.setAdapter(adapter);
     }
 }
