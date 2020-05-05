d81decd9444a375a3ce536a4c5607e75ee1b58d3
Rajkumar S <rajkumaar2304@icloud.com>
2020-05-05 16:37:39 +0530

    Modifies curriculum scraping (#41)
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>

--- app/src/main/java/in/co/rajkumaar/amritarepo/curriculum/CurriculumActivity.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/curriculum/CurriculumActivity.java
@@ -6,24 +6,29 @@ package in.co.rajkumaar.amritarepo.curriculum;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -32,8 +37,6 @@ import in.co.rajkumaar.amritarepo.R;
 import in.co.rajkumaar.amritarepo.activities.BaseActivity;
 import in.co.rajkumaar.amritarepo.helpers.DownloadTask;
 import in.co.rajkumaar.amritarepo.helpers.Utils;
-
-import static in.co.rajkumaar.amritarepo.helpers.Utils.THEME_LIGHT;
 
 public class CurriculumActivity extends BaseActivity {
 
@@ -49,96 +52,105 @@ public class CurriculumActivity extends BaseActivity {
         setContentView(R.layout.activity_curriculum);
 
         try {
-            webViewLink = getString(R.string.dev_domain) + "/utils/curriculum.php?theme=" + getSharedPreferences("theming", MODE_PRIVATE).getString("theme", THEME_LIGHT);
-            Log.e("LINK", webViewLink);
-            if (webViewLink == null) {
-                Utils.showToast(CurriculumActivity.this, "Invalid URL");
-                finish();
-                return;
-            }
+            webViewLink = getString(R.string.curriculum_url);
             this.setTitle("Curriculum");
             myWebView = findViewById(R.id.webView);
-            myWebView.getSettings().setJavaScriptEnabled(true);
             dialog = new ProgressDialog(CurriculumActivity.this);
+            myWebView.getSettings().setJavaScriptEnabled(true);
             myWebView.getSettings().setLoadWithOverviewMode(true);
             myWebView.getSettings().setUseWideViewPort(true);
             showProgressDialog();
-            myWebView.setWebChromeClient(new WebChromeClient() {
-                public void onProgressChanged(WebView view, int progress) {
-                    showProgressDialog();
-                    if (progress == 100)
-                        dismissProgressDialog();
-                }
-            });
             myWebView.setWebViewClient(new WebViewClient() {
-                @Nullable
-                @Override
-                public WebResourceResponse shouldInterceptRequest(WebView view, final String url) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                if (Utils.getUrlWithoutParameters(url).contains(".pdf")) {
-                                    final ArrayList<String> options = new ArrayList<>();
-                                    options.add("View");
-                                    options.add("Download");
-                                    AlertDialog.Builder optionsBuilder = new AlertDialog.Builder(CurriculumActivity.this);
-                                    ArrayAdapter<String> optionsAdapter = new ArrayAdapter<>(CurriculumActivity.this, android.R.layout.simple_list_item_1, options);
-                                    optionsBuilder.setAdapter(optionsAdapter, new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int pos) {
-                                            if (pos == 0) {
-                                                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url)));
-                                            } else if (pos == 1) {
-                                                if (ContextCompat.checkSelfPermission(CurriculumActivity.this,
-                                                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                                        != PackageManager.PERMISSION_GRANTED) {
-
-                                                    ActivityCompat.requestPermissions(CurriculumActivity.this,
-                                                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                            1);
-                                                } else {
-                                                    if (Utils.isConnected(CurriculumActivity.this)) {
-                                                        try {
-                                                            new DownloadTask(CurriculumActivity.this, Utils.getUrlWithoutParameters(url), 0);
-                                                        } catch (URISyntaxException e) {
-                                                            e.printStackTrace();
-                                                        }
-                                                    } else {
-                                                        Utils.showToast(CurriculumActivity.this, "Device not connected to Internet.");
-                                                    }
-
-
-                                                }
-                                            }
-                                        }
-                                    });
-                                    optionsBuilder.show();
-                                }
-                            } catch (URISyntaxException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-
-                    return super.shouldInterceptRequest(view, url);
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    if (url.startsWith(getString(R.string.amrita_file_prefix))) {
+                        showProgressDialog();
+                        getRealURLAndDisplayMenu(url);
+                    }
+                    return true;
                 }
 
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    try {
-                        return !Utils.getUrlWithoutParameters(url).contains(".pdf");
-                    } catch (URISyntaxException e) {
-                        e.printStackTrace();
-                        return true;
-                    }
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    dismissProgressDialog();
+                    super.onPageFinished(view, url);
                 }
             });
             myWebView.loadUrl(webViewLink);
         } catch (Exception e) {
             e.printStackTrace();
             finish();
-            Toast.makeText(CurriculumActivity.this, "Unexpected error. Please try again later", Toast.LENGTH_SHORT).show();
+            Utils.showUnexpectedError(CurriculumActivity.this);
         }
+    }
+
+    private void getRealURLAndDisplayMenu(String courseURL) {
+        showProgressDialog();
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, courseURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Document document = Jsoup.parse(response);
+                            String fileLink = document.select(".pdf-reader-download-link").first().select("a").attr("href");
+                            displayOptions(fileLink);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Utils.showUnexpectedError(CurriculumActivity.this);
+                            finish();
+                        } finally {
+                            dismissProgressDialog();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                dismissProgressDialog();
+                Utils.showUnexpectedError(CurriculumActivity.this);
+                finish();
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    private void displayOptions(final String url) {
+        dismissProgressDialog();
+        final ArrayList<String> options = new ArrayList<>();
+        options.add("View");
+        options.add("Download");
+        AlertDialog.Builder optionsBuilder = new AlertDialog.Builder(CurriculumActivity.this);
+        ArrayAdapter<String> optionsAdapter = new ArrayAdapter<>(CurriculumActivity.this, android.R.layout.simple_list_item_1, options);
+        optionsBuilder.setAdapter(optionsAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int pos) {
+                if (pos == 0) {
+                    startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url)));
+                } else if (pos == 1) {
+                    if (ContextCompat.checkSelfPermission(CurriculumActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(CurriculumActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                1);
+                    } else {
+                        if (Utils.isConnected(CurriculumActivity.this)) {
+                            try {
+                                new DownloadTask(CurriculumActivity.this, Utils.getUrlWithoutParameters(url), 0);
+                            } catch (URISyntaxException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            Utils.showToast(CurriculumActivity.this, "Device not connected to Internet.");
+                        }
+
+
+                    }
+                }
+            }
+        });
+        optionsBuilder.show();
     }
 
     public void dismissProgressDialog() {
