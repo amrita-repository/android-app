258208d56d5ffe50520f8a1b000165cd2858a785
Rajkumar <rajkumaar2304@icloud.com>
2023-12-23 15:49:04 -0700

    add user agent to prevent 403 in EResourcesActivity
    Signed-off-by: Rajkumar <rajkumaar2304@icloud.com>

--- app/src/main/java/in/co/rajkumaar/amritarepo/opac/EResourcesActivity.java
+++ app/src/main/java/in/co/rajkumaar/amritarepo/opac/EResourcesActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 RAJKUMAR S
+ * Copyright (c) 2023 RAJKUMAR S
  */
 
 package in.co.rajkumaar.amritarepo.opac;
@@ -8,14 +8,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -34,8 +31,8 @@ import in.co.rajkumaar.amritarepo.helpers.Utils;
 
 public class EResourcesActivity extends BaseActivity {
 
-    private Map<String, String> links = new HashMap<>();
-    private ArrayList<String> items = new ArrayList<>();
+    private final Map<String, String> links = new HashMap<>();
+    private final ArrayList<String> items = new ArrayList<>();
     private ListView listView;
 
     @Override
@@ -52,38 +49,34 @@ public class EResourcesActivity extends BaseActivity {
     void fetchResources(final boolean b) {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, getString(R.string.central_library),
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            Document doc = Jsoup.parse(response);
-                            Element table = doc.select("table").get(b ? 1 : 0);
-                            Elements anchors = table.select("a");
-                            for (Element a : anchors) {
-                                links.put(a.text(), a.attr("href"));
-                                items.add(a.text());
-                            }
-                            ArrayAdapter arrayAdapter = new ArrayAdapter<>(EResourcesActivity.this, R.layout.eresources_item, R.id.text1, items);
-                            listView.setAdapter(arrayAdapter);
-                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                @Override
-                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                                    intent.setData(Uri.parse(links.get(items.get(position))));
-                                    startActivity(intent);
-                                }
-                            });
-                            findViewById(R.id.progressBar).setVisibility(View.GONE);
-                        } catch (Exception e) {
-                            Utils.showUnexpectedError(EResourcesActivity.this);
+                response -> {
+                    try {
+                        Document doc = Jsoup.parse(response);
+                        Element table = doc.select("table").get(b ? 1 : 0);
+                        Elements anchors = table.select("a");
+                        for (Element a : anchors) {
+                            links.put(a.text(), a.attr("href"));
+                            items.add(a.text());
                         }
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(EResourcesActivity.this, R.layout.eresources_item, R.id.text1, items);
+                        listView.setAdapter(arrayAdapter);
+                        listView.setOnItemClickListener((parent, view, position, id) -> {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setData(Uri.parse(links.get(items.get(position))));
+                            startActivity(intent);
+                        });
+                        findViewById(R.id.progressBar).setVisibility(View.GONE);
+                    } catch (Exception e) {
+                        Utils.showUnexpectedError(EResourcesActivity.this);
                     }
-                }, new Response.ErrorListener() {
+                }, error -> Utils.showUnexpectedError(EResourcesActivity.this)) {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Utils.showUnexpectedError(EResourcesActivity.this);
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
+                params.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15");
+                return params;
             }
-        });
+        };
         requestQueue.add(stringRequest);
     }
 }
